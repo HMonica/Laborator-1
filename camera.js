ef4b6e26e7206eed096f02104ba355600537d499
HMonica <60745094+HMonica@users.noreply.github.com>
2021-02-10 20:11:11 +0200

    Update camera.js

--- camera.js
+++ camera.js
@@ -1,32 +1,87 @@
-var video = document.getElementById("video");
+var synth = window.speechSynthesis;
 
-//---------
+var inputForm = document.querySelector('form');
+var inputTxt = document.querySelector('.txt');
+var voiceSelect = document.querySelector('select');
 
-function on_cam_success(stream) {
-    video.srcObject = stream;
+var pitch = document.querySelector('#pitch');
+var pitchValue = document.querySelector('.pitch-value');
+var rate = document.querySelector('#rate');
+var rateValue = document.querySelector('.rate-value');
+
+var voices = [];
+
+function populateVoiceList() {
+  voices = synth.getVoices().sort(function (a, b) {
+      const aname = a.name.toUpperCase(), bname = b.name.toUpperCase();
+      if ( aname < bname ) return -1;
+      else if ( aname == bname ) return 0;
+      else return +1;
+  });
+  var selectedIndex = voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
+  voiceSelect.innerHTML = '';
+  for(i = 0; i < voices.length ; i++) {
+    var option = document.createElement('option');
+    option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+    
+    if(voices[i].default) {
+      option.textContent += ' -- DEFAULT';
+    }
+
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
+    voiceSelect.appendChild(option);
+  }
+  voiceSelect.selectedIndex = selectedIndex;
 }
 
-function on_cam_error(err) {
-    alert("error." + err.message);
+populateVoiceList();
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
 }
 
-//----------
+function speak(){
+    if (synth.speaking) {
+        console.error('speechSynthesis.speaking');
+        return;
+    }
+    if (inputTxt.value !== '') {
+    var utterThis = new SpeechSynthesisUtterance(inputTxt.value);
+    utterThis.onend = function (event) {
+        console.log('SpeechSynthesisUtterance.onend');
+    }
+    utterThis.onerror = function (event) {
+        console.error('SpeechSynthesisUtterance.onerror');
+    }
+    var selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+    for(i = 0; i < voices.length ; i++) {
+      if(voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+        break;
+      }
+    }
+    utterThis.pitch = pitch.value;
+    utterThis.rate = rate.value;
+    synth.speak(utterThis);
+  }
+}
 
-var constraints = {audio: false, video: true};
-navigator.mediaDevices.getUserMedia(constraints)
-.then(on_cam_success)
-.catch(on_cam_error);
+inputForm.onsubmit = function(event) {
+  event.preventDefault();
 
+  speak();
 
-video.addEventListener("touchstart",capteaza);
-video.addEventListener("mousedown",capteaza);
+  inputTxt.blur();
+}
 
-//----------
+pitch.onchange = function() {
+  pitchValue.textContent = pitch.value;
+}
 
-function capteaza() {
-    var c = document.getElementById("canvas");
-    c.width = video.width;
-    c.height = video.height;
-    var ctx = c.getContext("2d");
-    ctx.drawImage(video,0,0,640,480);
+rate.onchange = function() {
+  rateValue.textContent = rate.value;
+}
+
+voiceSelect.onchange = function(){
+  speak();
 }
